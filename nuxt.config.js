3afd8318170f1247a929e44bad751ddacdb8b033
Edouard Duplessis <eduplessis@gmail.com>
2018-05-16 16:43:03 -0400

    removing nuxtent

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,17 +1,43 @@
+var glob = require('glob');
+var path = require('path');
+
+// Enhance Nuxt's generate process by gathering all content files from Netifly CMS
+// automatically and match it to the path of your Nuxt routes.
+// The Nuxt routes are generate by Nuxt automatically based on the pages folder.
+var dynamicRoutes = getDynamicPaths({
+  '/projets': 'blog/projets/*.json'
+});
+
 module.exports = {
-  modules: ["nuxtent", "nuxt-netlify-cms"],
-  nuxtent: {
-    content: {
-      page: "/_post",
-      permalink: ":year/:slug",
-      generate: [
-        // assets to generate static build
-        "get",
-        "getAll"
-      ]
-    }
-  },
+  modules: ["nuxt-netlify-cms"],
   css: [
     '@/assets/css/main.scss'
-  ]
+  ],
+  generate: {
+    routes: dynamicRoutes
+  },
+  head: {
+    title: 'Plp Constructions inc.',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'Plp Constructions inc.' }
+    ]
+  },
 };
+
+
+/**
+ * Create an array of URLs from a list of files
+ * @param {*} urlFilepathTable
+ */
+function getDynamicPaths(urlFilepathTable) {
+  return [].concat(
+    ...Object.keys(urlFilepathTable).map(url => {
+      var filepathGlob = urlFilepathTable[url];
+      return glob
+        .sync(filepathGlob, { cwd: 'content' })
+        .map(filepath => `${url}/${path.basename(filepath, '.json')}`);
+    })
+  );
+}
---
+++ pages/projets/_projet.vue
@@ -0,0 +1,16 @@
+<template>
+<div>
+    <h1>{{ title }}</h1>
+		<div v-html="body" />
+    <img :src="image" alt="">
+</div>
+</template>
+
+<script>
+export default {
+  async asyncData({ params }) {
+    let post = await import('~/content/projets/' + params.projet + '.json');
+    return post;
+  }
+}
+</script>
